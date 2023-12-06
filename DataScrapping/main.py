e632f93774f0435c42d528aa19172ea296b6da2c
Max <bomax1597@gmail.com>
2023-12-06 15:39:41 +0100

    removed parasite files

--- DataScrapping/main.py
+++ DataScrapping/main.py
@@ -39,54 +39,36 @@ if __name__ == "__main__":
         results = driver.find_element(By.CLASS_NAME, "main-col")
         results = results.find_element(By.CLASS_NAME, "list-categorie")
         results = results.find_elements(By.CSS_SELECTOR, "li")
-        for result in results:
-            print(len(results))
+        size = len(results)
+        for result_i in range(size):
+            results = driver.find_element(By.CLASS_NAME, "main-col")
+            results = results.find_element(By.CLASS_NAME, "list-categorie")
+            results = results.find_elements(By.CSS_SELECTOR, "li")
+            result = results[result_i]
             if is_in('-', result.text):
-                pass
+                continue
             # / Jurisprudence => list des jurisprudences par catregorie
             link_to_all_jp = result.find_element(By.CSS_SELECTOR, "a")
             link_to_all_jp.click()
-            jp_main = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "main_constit"))
+            pages = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "pager-item"))
             )
-            articles = jp_main.find_elements(By.CLASS_NAME, "result-item")
-            for article in articles:
-                # print(article.text)
-                link_to_jp = article.find_element(By.CSS_SELECTOR, "a")
+            nb_pages = int(pages[-1].text)
+            for page_i in range(nb_pages - 1):
+                jp_main = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "main-col"))
+                )
+                articles = jp_main.find_elements(By.CLASS_NAME, "result-item")
+                articles_nb = len(articles)
+                for article_i in range(articles_nb):
+                    jp_main = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.CLASS_NAME, "main-col"))
+                    )
+                    articles = jp_main.find_elements(By.CLASS_NAME, "result-item")
+                    article = articles[article_i]
+                    link_to_jp = article.find_element(By.CSS_SELECTOR, "a")
             driver.back()
             # \ Jurisprudence
-        """
-        for result in results:
-            link = result.find_element(By.CSS_SELECTOR, "a")
-            code = result.text
-            # / Code => liste des chapitres contenant les articles
-            link.click()
-            articles = WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "articleLink"))
-            )
-            for article in articles:
-                if article.text[:-1] in articles_reg:
-                    continue
-                # / Articles => liste de quelques articles
-                article.click()
-                articles_list = WebDriverWait(driver, 10).until(
-                    EC.presence_of_all_elements_located((By.ID, "liste-article-noeud"))
-                )
-                for art in articles_list:
-
-                    article_num = art.find_elements(By.CLASS_NAME, "name-article")
-                    article_desc = art.find_elements(By.CLASS_NAME, "content")
-                    for num, desc in zip(article_num, article_desc):
-                        formatted = format_json(code, num.text, desc.text)
-                        print(formatted)
-                        data.append(formatted)
-                        articles_reg.append(num.text)
-
-                driver.back()
-                # \ Articles
-            driver.back()
-            # \ Code
-        """
     finally:
         driver.quit()
         push_to_json(data)
